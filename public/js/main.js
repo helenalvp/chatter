4a4169f54063673120aada4da0bc5b17484c8657
helenalvp <39470670+helenalvp@users.noreply.github.com>
2021-08-08 21:46:22 -0400

    finished walktrhough of video

--- public/js/main.js
+++ public/js/main.js
@@ -1,90 +1,64 @@
-const chatForm = document.getElementById('chat-form');
-const chatMessages = document.querySelector('.chat-messages');
-const roomName = document.getElementById('room-name');
-const userList = document.getElementById('users');
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
+//Get username and room from url
 
-// Get username and room from URL
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
 
 const socket = io();
 
-// Join chatroom
-socket.emit('joinRoom', { username, room });
+//Join chat room
+socket.emit("joinRoom", { username, room });
 
-// Get room and users
-socket.on('roomUsers', ({ room, users }) => {
+//Get room and users
+socket.on("roomUsers", ({ room, users }) => {
   outputRoomName(room);
   outputUsers(users);
 });
 
-// Message from server
-socket.on('message', (message) => {
+//Message from server
+socket.on("message", (message) => {
   console.log(message);
   outputMessage(message);
 
-  // Scroll down
   chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
-// Message submit
-chatForm.addEventListener('submit', (e) => {
+//Message Submit
+chatForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  // Get message text
-  let msg = e.target.elements.msg.value;
+  //Get Message Text
+  const msg = e.target.elements.msg.value;
 
-  msg = msg.trim();
+  //Emit message to server
+  socket.emit("chatMessage", msg);
 
-  if (!msg) {
-    return false;
-  }
-
-  // Emit message to server
-  socket.emit('chatMessage', msg);
-
-  // Clear input
-  e.target.elements.msg.value = '';
-  e.target.elements.msg.focus();
+  //Clear Input
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.value.focus();
 });
 
-// Output message to DOM
-function outputMessage(message) {
-  const div = document.createElement('div');
-  div.classList.add('message');
-  const p = document.createElement('p');
-  p.classList.add('meta');
-  p.innerText = message.username;
-  p.innerHTML += `<span>${message.time}</span>`;
-  div.appendChild(p);
-  const para = document.createElement('p');
-  para.classList.add('text');
-  para.innerText = message.text;
-  div.appendChild(para);
-  document.querySelector('.chat-messages').appendChild(div);
+//Output message to DOM
+function outputMessage(messageObj) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<p class="meta">${messageObj.username} <span>${messageObj.time}</span></p>
+                    <p class="text">${messageObj.text}</p>`;
+  document.querySelector(".chat-messages").appendChild(div);
 }
 
-// Add room name to DOM
+//Add room name to DOM
 function outputRoomName(room) {
   roomName.innerText = room;
 }
 
-// Add users to DOM
+//Add users to DOM
 function outputUsers(users) {
-  userList.innerHTML = '';
-  users.forEach((user) => {
-    const li = document.createElement('li');
-    li.innerText = user.username;
-    userList.appendChild(li);
-  });
+  userList.innerHTML = `${users
+    .map((user) => `<li>${user.username}</li>`)
+    .join("")}`;
 }
-
-//Prompt the user before leave chat room
-document.getElementById('leave-btn').addEventListener('click', () => {
-  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
-  if (leaveRoom) {
-    window.location = '../index.html';
-  } else {
-  }
-});
